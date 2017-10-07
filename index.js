f9ada844cebdb6b7f56300a158604488b01856d7
Stephen Harding <StephenHarding79@gmail.com>
2017-10-07 17:25:45 -0400

    started baddies

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ var position = {
   sLeft: 0,
   sTop: 0
 }
+var level = 1
+
 var area = document.getElementById('area')
 
 document.addEventListener("keydown", function(e){
-  console.log(e)
   if (e.keyCode === 39) {
     if (position.pLeft >= 2060){
       return
@@ -21,22 +22,16 @@ document.addEventListener("keydown", function(e){
       position.aLeft -= 20
       player.style.left = position.pLeft + 20 + 'px'
       position.pLeft += 20
-      console.log("position pLeft" + position.pLeft)
-      console.log("position aLeft" + position.aLeft)
     }
     else {
       player.style.left = position.pLeft + 20 + 'px'
       position.pLeft += 20
-      console.log("position pLeft" + position.pLeft)
-      console.log("position aLeft" + position.aLeft)
     }
   }
    if (e.keyCode === 37) {
     if (position.pLeft <= 0) {
       return
     }
-    console.log('position pLeft' + position.pLeft)
-    console.log('position aLeft' + position.aLeft)
     if (position.pLeft - ((-1)*position.aLeft) < 140 && (position.pLeft > 80 && position.pLeft < 1620)) {
       area.style.left = position.aLeft + 20 + 'px'
       position.aLeft += 20
@@ -46,16 +41,12 @@ document.addEventListener("keydown", function(e){
     else {
       player.style.left = position.pLeft - 20 + 'px'
       position.pLeft -= 20
-          console.log('position pLeft' + position.pLeft)
-    console.log('position aLeft' + position.aLeft)
     }
   }
   if (e.keyCode === 38) {
     if (position.pTop <= 0) {
       return
     }
-    console.log('position pTop' + position.pTop)
-    console.log('position aTop' + position.aTop)
     if (position.pTop - ((-1)*position.aTop) < 140 && (position.pTop > 80 && position.pTop < 1620)) {
       area.style.top = position.aTop + 20 + 'px'
       position.aTop += 20
@@ -76,15 +67,51 @@ document.addEventListener("keydown", function(e){
       position.aTop -= 20
       player.style.top = position.pTop + 20 + 'px'
       position.pTop += 20
-      console.log("position pTop" + position.pTop)
-      console.log("position aTop" + position.aTop)
     }
     else {
       player.style.top = position.pTop + 20 + 'px'
       position.pTop += 20
-      console.log("position pTop" + position.pTop)
-      console.log("position aTop" + position.aTop)
     }
   }
 })
+    var random = function() {
+      if(Math.floor(Math.random() * 2) === 1) {
+        return (-1)
+      }
+      else {
+        return 1
+      }
+    }
+if (level === 1) {
+  var baddies = ['baddie0','baddie1','baddie2']
+  baddies.forEach(function(b) {
+    $("#area").append(`<div id=${b} class ='baddie'></div>`)
+  })
+  $('.baddie').css({"background-color" : "black" , "width" : "40px", "height" : "40px",
+   "position" : "absolute"})
+  $('.baddie').each(function(b){
+      $(this).css({"left" : `${Math.floor(Math.random()*2060)}px`, "top" : `${Math.floor(Math.random()*2060)}px`})
+      var x = Math.random()*(21 - (-20)) + (-20)
+      if (x > 20) {
+        x = 20
+      }
+      var y = (random())*Math.floor(Math.sqrt(400 - (x**2)))
+      x = Math.floor(x)
+      $(this).append( `<p id='${b}x' class="bx">${x}</p>`)
+      $(this).append( `<p id='${b}y' class="by">${y}</p>`)
+      console.log(x)
+      console.log(y)
+  })
+}
+
+const timer = function() {
+  $('.baddie').each(function(){
+    $p = $(this).position()
+    console.log($(this)[0].html())
+    $(this).css({'left' : `4px`})
+  })
+
+}
+window.setInterval(timer, 1000)
+
 
